0bedeed67fb97173f69c5a6d59c27c98bd935186
Bibhavshah <202051212@iiitvadodara.ac.in>
2023-11-24 22:57:17 +0530

    changed url

--- src/components/Body/VideoPlayer.jsx
+++ src/components/Body/VideoPlayer.jsx
@@ -6,7 +6,7 @@ const VideoPlayer = () => {
       <iframe
         width="1000" // Set your desired width
         height="500" // Set your desired height
-        src={`https://www.youtube.com/embed/wN6OFpy93TE`}
+        src={`https://www.youtube.com/embed/6a1fLEToyvU`}
         allowFullScreen
       />
     </div>
